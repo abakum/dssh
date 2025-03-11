504e6b3dc9387f99cbcb67963546598da31d1624
abakum <koka.abakum@gmail.com>
2025-03-11 12:09:22 +0300

    Update viewLinuxServerOverDirectClient.bat

--- viewLinuxServerOverDirectClient.bat
+++ viewLinuxServerOverDirectClient.bat
@@ -1,6 +1,5 @@
 set client=-j direct.accesible.dssh
 :set client=-J direct.accesible.sshd -j direct.accesible.dssh
-set client=-j ssh.cloudns.ch
 set server=:
 set listen=_
 set/p p=Run `dssh` on VNC server. Press Enter
